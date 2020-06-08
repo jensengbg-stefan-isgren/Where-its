6008f6551c162f5893bf0036eb3dcd58b0a9b3a0
Marcus Edvardsson <54709946+jensengbg-marcus-edvardsson@users.noreply.github.com>
2020-06-08 12:33:33 +0200

    skapade views
    skapade views med tillhörande paths

---
+++ src/views/Admin.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="admin-wrapper">
+        <img src="../assets/logologo.png" alt="logo">
+    </div>
+</template>
+
+<style scoped>
+.admin-wrapper {
+    height: 100vh;
+    width: 100vw;
+    background: #231F42;
+}
+</style>
