3a4884d29d23eaa10040f939a1dcba4b639a10b3
Johanna Jansson <joja92@hotmail.com>
2020-06-08 13:43:17 +0200

    first commit
    Probem with removing node_modules. Will try and fix it later

---
+++ src/components/eventItem.vue
@@ -0,0 +1,33 @@
+<template>
+    <article class="event-item">
+        <div class="event-item-date">
+            <p class="event-item-date-day">21</p>
+            <p class="event-item-date-month">mar</p>
+        </div>
+        <h2 class="event-item-name">Lasse Stefans</h2>
+        <p class="event-item-place">Kjellsalen</p>
+        <p class="event-item-time">19.00 - 21.00</p>
+        <p class="event-item-price">350 sek</p>
+    </article>
+</template>
+
+<script>
+export default {
+    name: 'eventItem'
+}
+</script>
+
+<style lang="scss" scoped>
+    .event-item {
+        color: #ffffff;
+
+        &-date {
+            border: 1px solid #ffffff;
+            border-radius: 0.5rem;
+        }
+        &-price {
+            color: #37AEAB;
+        }
+    }
+
+</style>
---
+++ src/views/Events.vue
@@ -0,0 +1,21 @@
+<template>
+    <main class="main-container">
+        <h1>Events</h1>
+        <div class="event-container">
+
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+    .main-container {
+        background: #231F42;
+    }
+
+</style>
