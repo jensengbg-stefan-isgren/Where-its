23982de50f7f273e7f6835910784abe276979a7b
Marcus Edvardsson <54709946+jensengbg-marcus-edvardsson@users.noreply.github.com>
2020-06-08 13:14:26 +0200

    editrs
    edits

---
+++ src/components/addEvent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="addEvent-wrapper">
+        <form action="#">
+            <input type="text" name="name" id="name" placeholder="Name">
+            <input type="text" name="where" id="where" placeholder="Where">
+            <input type="date" name="date" id="date" placeholder="When">
+            <input type="time" name="" id="">
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'addEvent'
+}
+</script>
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,13 +1,28 @@
 <template>
     <div class="admin-wrapper">
-        <img src="../assets/logologo.png" alt="logo">
+        <img src="../assets/logologo.png" alt="logo" class="logo">
+        <addEvent />
     </div>
 </template>
+
+<script>
+import addEvent from '../components/addEvent'
+export default {
+    name: 'Admin',
+    components: {
+        addEvent
+    }
+}
+</script>
 
 <style scoped>
 .admin-wrapper {
     height: 100vh;
     width: 100vw;
     background: #231F42;
+}
+
+.logo {
+    margin: 2%;
 }
 </style>
