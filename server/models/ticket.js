8d2c659470c68022b4848370d07a403b7f4724a1
Matilda Höglund <48841764+jensengbg-matilda-hoglund@users.noreply.github.com>
2020-06-08 12:19:39 +0200

    upd ticketModel

--- server/models/ticket.js
+++ server/models/ticket.js
@@ -1,11 +1,15 @@
 const mongoose = require("mongoose");
 
 const ticketSchema = new mongoose.Schema({
+  ticket_number: {
+    type: Number
+    // kanske behövs om front-end gör ett ticketNumber
+  },
   event_id: {
     type: mongoose.Schema.Types.ObjectId,
     required: true
   },
-  createdTime: {
+  created_at: {
     type: Date,
     required: true
   },
