8e66acd5f8f9a05b369f3463101f5ad69b4ec67c
Matilda Höglund <48841764+jensengbg-matilda-hoglund@users.noreply.github.com>
2020-06-08 13:11:19 +0200

    upd error-handling

--- server/controllers/ticketController.js
+++ server/controllers/ticketController.js
@@ -1,32 +1,35 @@
 const mongoose = require("mongoose"),
-  db = mongoose.connection,
   Event = require("../models/event"),
   Ticket = require("../models/ticket");
 
 exports.addTicket = async (req, res) => {
-  const user_id = req.body.user_id,
-    event_id = req.body.event_id,
-    date = new Date();
+  try {
+    const user_id = req.body.user_id,
+      event_id = req.body.event_id,
+      date = new Date();
 
     let sold_tickets = req.body.sold_tickets;
-    sold_tickets ++;
+    sold_tickets++;
 
-  // Upd antal sålda biljetter
-  Event.updateOne(
-    { _id: event_id },
-    { $set: { sold_tickets: sold_tickets } },
-    (err) => {
-      if (err) console.log(err);
-    }
-  );
+    // Upd antal sålda biljetter
+    Event.updateOne(
+      { _id: event_id },
+      { $set: { sold_tickets: sold_tickets } },
+      (err) => {
+        if (err) console.log(err);
+      }
+    );
 
-  // Skapa ny ticket
-  const ticket = new Ticket({
-    event_id: event_id,
-    created_at: date,
-    user_id: user_id,
-  });
-  ticket.save();
+    // Skapa ny ticket
+    const ticket = new Ticket({
+      event_id: event_id,
+      created_at: date,
+      user_id: user_id,
+    });
+    ticket.save();
+  } catch (err) {
+    console.log(err);
+  }
 
   res.send(ticket);
 };
