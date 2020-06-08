56eef9f01029aa24de8ad5d4df3608d63b8539ef
Matilda Höglund <48841764+jensengbg-matilda-hoglund@users.noreply.github.com>
2020-06-08 12:02:20 +0200

    upd ticketController

---
+++ server/controllers/ticketController.js
@@ -0,0 +1,24 @@
+const express = require("express"),
+bodyParser = require("body-parser"),
+mongoose = require("mongoose"),
+router = express.Router(),
+db = mongoose.connection,
+ticketSchema = db.ticketSchema;
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+
+router.post("/ticket", async (req, res) => {
+    const user_id = req.body.user_id,
+    event_id = req.body.event_id,
+    date = new Date();
+  
+    const ticket = new Ticket({
+      event_id: event_id,
+      created_at: date,
+      user_id: user_id
+    });
+  
+    ticket.save();
+    res.send(ticket);
+  });
